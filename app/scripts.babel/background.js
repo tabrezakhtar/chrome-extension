adb16a178bbbbde33f1435ac96a2144d4c11b7da
tabrezakhtar <t_akhtar232@MisterT-Pro.lan>
2016-07-03 15:05:32 +0100

    added tab events

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,5 +1,35 @@
 'use strict';
 
-chrome.browserAction.onClicked.addListener( tab => {
-    alert('hi');
+var tabId;
+
+chrome.browserAction.onClicked.addListener( listener => {
+
+    if (tabId) {
+
+        chrome.tabs.update(tabId, {
+            selected:true
+        }, tab => {
+            console.log('opened same tab');
+        });
+
+    }
+    else {
+
+        chrome.tabs.create( {
+            url:chrome.extension.getURL('popup.html'),
+            selected:true
+        }, tab => {
+            tabId = tab.id;
+            console.log('new tab ' + tabId);
+        });
+
+    }
+
+});
+
+chrome.tabs.onRemoved.addListener(function(closedTabId) {
+    if (closedTabId === tabId) {
+        tabId = null;
+    }
+    console.log('removed tab ' + tabId);
 });
